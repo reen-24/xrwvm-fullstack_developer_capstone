75018fede3e7697181e82fc7971c5b76cdbed36e
reen-24 <reenasonigrah19@gmail.com>
2024-08-24 17:46:58 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,13 +6,3 @@ from .models import CarMake, CarModel
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
